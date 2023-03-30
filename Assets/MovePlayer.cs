8dd33b085bd1c65b895c021287a4ea7e2041d26e
TotiVasilev <todortihomirov01@abv.bg>
2023-03-30 07:48:59 +0200

    Animator

--- Assets/MovePlayer.cs
+++ Assets/MovePlayer.cs
@@ -1,10 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MovePlayer : MonoBehaviour
 {
+    [Header("Events")]
+	[Space]
+
+	public UnityEvent OnLandEvent;
+
+	[System.Serializable]
+	public class BoolEvent : UnityEvent<bool> { }
     private float horizontal;// A,D || < > movement
+    private float vertical;
     public float speed = 8f;
     public float jumpingPower = 16f;
     private bool isFacingRight = true;//sprite profile = horizontal direction
@@ -15,40 +25,75 @@ public class MovePlayer : MonoBehaviour
     private float jumpBufferTime = 0.15f;// registering jump shortly before entering the platform
     private float jumpBufferCounter;
 
+    public Animator animator;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;//every collision with object in this layer = IsGrounded
+    bool run; 
+    public AudioSource foosteps;
 
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
 
     //Checking if we can jump
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);//creating a sphere around the empty grouch check object
+        
     }
+
+    void StartRun()
+        {
+            if(run)
+            {
+              animator.SetFloat("Running", Math.Abs(horizontal));
+            }
+        }
+        
 
     void Update()
     {
         //Horizontal movement
         horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw ("Vertical");
+        
+       
+        
         Flip();//sprite update
 
+        StartRun();
 
         if (IsGrounded())//Coyote update
         {
+            
+            animator.SetFloat("Running", Math.Abs(horizontal));
+            
+            animator.SetBool("Jumping", false);
+
             coyoteTimeCounter = coyoteTime;// = 0, grounded
+            
         }
         else
         {
+            animator.SetFloat("Running", Math.Abs(0));
             coyoteTimeCounter -= Time.deltaTime;// leaving the ground, start counting for coyote time
+            animator.SetBool("Jumping", true);
+            
         }
 
         if (Input.GetButtonDown("Jump"))//Jump buffer update
         {
             jumpBufferCounter = jumpBufferTime;
+            
+            
         }
         else
         {
             jumpBufferCounter -= Time.deltaTime;
+            
         }
 
         //Reseting jump buffer after jumping 
@@ -62,15 +107,23 @@ public class MovePlayer : MonoBehaviour
         //Reseting coyote time after jumping
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
+            animator.SetBool("Jumping", true);
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
 
             coyoteTimeCounter = 0f;
         }
     }
 
+   public void OnLanding()
+   {
+    
+  
+   }
+
     private void FixedUpdate()// physics in Fixed update
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        
     }
 
     private void Flip()
